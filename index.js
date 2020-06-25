2dbaf7e7d8ba6be182e5338adfb7238a5a9d5613
FranG98 <motherofsoad@hotmail.com>
2020-06-24 21:32:12 -0300

    Subida de Backend del proyecto

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+const { mongoose } = require('./database');
+//Configuracion
+app.set('port', process.env.PORT || 3000);
+//Middlewares
+app.use(express.json());
+app.use(cors({origin: 'http://localhost:4200'}));
+//Rutas
+
+//Inicio
+app.listen(app.get('port'), () =>{
+    console.log("Server iniciado en puerto ",app.get('port'));
+})
