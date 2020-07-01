b4982c5a77e7850ad7dca31ab4c2c244b4dea3cf
LucasChoqueV <lucaasbruu@gmail.com>
2020-07-01 20:35:24 -0300

    agregando noticia

--- models/noticia.js
+++ models/noticia.js
@@ -4,6 +4,7 @@ const { Schema } = mongoose;
 
 const NoticiaSchema = new Schema ({
     titulo: {type: String, required: true},
+    imagen: {type: String, required: true},
     descripcion: {type: String, required: true},
     fecha: {type: Date, default: Date.now()},
     usuario: {type: Schema.Types.ObjectId, ref: Usuario},
